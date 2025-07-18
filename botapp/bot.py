3e721af0545e59b1638c6390123574e2565c9b81
Muhammadyor Vohidov <muhammadyor.2022@gmail.com>
2025-07-18 15:33:43 +0500

    Initial commit

--- botapp/bot.py
+++ botapp/bot.py
@@ -1,8 +1,9 @@
 import os
 import django
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext
 from decouple import config
+from asgiref.sync import sync_to_async
 
 # Django setup
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -12,16 +13,40 @@ from botapp.models import TelegramUser
 
 BOT_TOKEN = config('BOT_TOKEN')
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.message.from_user
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     user = update.message.from_user
+#     TelegramUser.objects.get_or_create(
+#         user_id=user.id,
+#         defaults={
+#             'username': user.username,
+#             'first_name': user.first_name,
+#         }
+#     )
+#     await update.message.reply_text("👋 Hello! You're now connected to Django!")
+
+@sync_to_async
+def get_or_create_user(user):
     TelegramUser.objects.get_or_create(
         user_id=user.id,
-        default={
+        defaults={
             'username': user.username,
             'first_name': user.first_name,
         }
     )
-    await update.message.reply_text("👋 Hello! You're now connected to Django!")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    await get_or_create_user(user)
+    await update.message.reply_text(f"👋 Hello, {user.first_name}! You're now connected to Django!")
+    text = (
+        f"🧾 User Info:\n"
+        f"ID: {user.id}\n"
+        f"Username: @{user.username}\n"
+        f"Name: {user.first_name} {user.last_name or ''}\n"
+        f"Language: {user.language_code}\n"
+        f"Premium: {user.is_premium}\n"
+    )
+    await update.message.reply_text(text)
 
 def run_bot():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
--- botapp/models.py
+++ botapp/models.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
@@ -5,6 +7,11 @@ class TelegramUser(models.Model):
     user_id = models.BigIntegerField(unique=True)
     username = models.CharField(max_length=150, null=True, blank=True)
     first_name = models.CharField(max_length=150, null=True, blank=True)
+    last_name = models.CharField(max_length=150, null=True, blank=True)
+    language = models.CharField(max_length=26, null=True, blank=True)
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} ({self.username})"
